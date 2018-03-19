efa5d8928fc097930c96312245a17b0b53177588
Filiberto Garcia mendoza <filiberto.garciamendoza@stu.jefferson.kyschools.us>
2018-03-19 08:31:15 -0400

    filiberto and brian

---
+++ codehs.py
@@ -0,0 +1,9 @@
+# Filiberto garcia and Bian Robledo
+fortnite = {
+    "fortnite": "there are 3.4 million player who play fortnite",
+    "fortnite_building": "in fortnite player can build wall, stair and also house trap for other player",
+    "fortnite surviving": "in fortnite in order to survive you have to land on place to get gun that they are spawn inside building also get inside the safe zone",
+    "fortnite" : "The Battle Is Building! Fortnite Battle Royale is the FREE 100-player PvP mode in Fortnite",
+    "fortnite" : "Today we're talking to Ali 'Myth' Kabbani, 'Fortnite' team captain for Team Solomid and a recent member of the 1000000 followers club",
+    "fortnite" : "Fortnite on mobile is still in its very early stages and it’ll be a while before absolutely everyone can access",
+    }
