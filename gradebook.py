ba3e33a9213c0e4e98c354d7abbb0c3364acf7fe
Filiberto Garcia mendoza <filiberto.garciamendoza@stu.jefferson.kyschools.us>
2018-03-28 08:24:05 -0400

    JJ

---
+++ gradebook.py
@@ -0,0 +1,5 @@
+computer_science = input ("what is your computer_science grade ")
+math = input ("what your math grade")
+ESL = input ("what your esl grade")
+science = input ("what you grade in science")
+humanities = input ("what your grade in your humanities")
